f9a6b4c407c321356be7c7f40036cb7792f184e8
anastasiabudanova <114395124+bisho4ka@users.noreply.github.com>
2024-11-07 11:56:08 +0300

    Update checkingTheString.cpp

--- src/checkingTheString.cpp
+++ src/checkingTheString.cpp
@@ -1,5 +1,16 @@
 #include "../include/checkingTheString.h"
 
 bool checkingTheString(const std::string &str) {
-  
+    int open = 0, close = 0;
+    
+    if (str.size() == 0) return true;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == '(') open += 1;
+        else if (str[i] == ')') close += 1;
+
+        if (close > open) break;
+    }
+
+    return open == close;
 }
