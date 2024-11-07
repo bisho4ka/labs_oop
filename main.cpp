ae037264a48227d10251f5074210ef35748558bb
anastasiabudanova <114395124+bisho4ka@users.noreply.github.com>
2024-11-08 01:23:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include "../include/checkingTheString.h"
 
+int main {
+    std::string str;
+
+    std::cout << "Введите строку: ";
+    std::cin >> str;
+
+    try {
+        std::cout << "Результат: " << checkingTheString(str) << std::endl;
+    }
+    catch(std::invalid_argument error) {
+        std::cout << "Произошла ошибка: " << error.what() << endl;
+    }
+
+    return 0;
+}
