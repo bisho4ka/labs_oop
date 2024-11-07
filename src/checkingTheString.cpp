8a9eca86a075068268a684c7898e0d3a99f6deb2
anastasiabudanova <114395124+bisho4ka@users.noreply.github.com>
2024-11-07 12:28:36 +0300

    Update checkingTheString.cpp

--- src/checkingTheString.cpp
+++ src/checkingTheString.cpp
@@ -9,6 +9,7 @@ bool checkingTheString(const std::string &str) {
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == '(') open += 1;
         else if (str[i] == ')') close += 1;
+        else throw std::invalid_argument("The string must consist only of '(' and ')'");
 
         if (close > open) break;
     }
